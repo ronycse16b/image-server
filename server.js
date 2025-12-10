3b980c982d844aa1b1831226f968b947a1734135
ronycse16b <rony.cse16b@gmail.com>
2025-12-10 18:39:17 +0600

    update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const UPLOADS_DIR = process.env.UPLOADS_DIR || "uploads";
 const UPLOADS_URL =
   process.env.UPLOADS_URL || "https://cdn.soulcraftbd.com/uploads";
 const ALLOWED_ORIGINS = (
-  process.env.ALLOWED_ORIGINS || "https://soulcraftbd.com"
+  process.env.ALLOWED_ORIGINS || "https://soulcraftbd.com" || "http://localhost:3000"
 ).split(",");
 const MAX_FILES = parseInt(process.env.MAX_FILES || "20");
 const MAX_FILE_SIZE = parseInt(process.env.MAX_FILE_SIZE || 2 * 1024 * 1024);
